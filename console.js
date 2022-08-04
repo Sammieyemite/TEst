522077e36556014ae948fb6eb72218905f94cb26
Olayemi Fatogun <sammieyemite@gmail.com>
2022-08-04 15:09:57 +0100

    first commit

---
+++ console.js
@@ -0,0 +1,3 @@
+console.log("-------");
+console.log("rise and shine");
+console.log(5 + 7);
---
+++ script.js
@@ -0,0 +1,43 @@
+// primitive data types
+
+// number
+var age = 23;
+// console.log(age);
+
+
+// string
+var firstName = "John";
+// console.log(firstName)
+
+// boolean
+var isUserLoggedIn = false;
+// console.log(isUserLoggedIn)
+
+
+// null
+var thing = null
+// console.log(thing)
+
+// undefined
+var letter;
+// console.log(letter)
+
+
+// Variable scope
+
+var arr = "global";
+
+
+
+function checkScope(){
+    var myArr = "local";
+    document.write(myArr);
+}
+
+checkScope();
+
+
+// document.write(arr);
+
+var boolean = true
+console.log(boolean)
